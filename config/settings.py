65605cb9785a1f99ee43836598a2462a9147ab56
Deepak-ctrl-star <deepakkrishnan451@gmail.com>
2026-07-07 21:28:33 +0530

    Create settings.py

---
+++ config/settings.py
@@ -0,0 +1,17 @@
+# Podcast RSS Feed
+RSS_URL = ""
+
+# Seconds to skip from the beginning
+SKIP_START_SECONDS = 180
+
+# Seconds to skip from the end
+SKIP_END_SECONDS = 180
+
+# Minimum clip duration
+MIN_CLIP_LENGTH = 45
+
+# Maximum clip duration
+MAX_CLIP_LENGTH = 60
+
+# Number of clips to generate
+NUMBER_OF_CLIPS = 2
